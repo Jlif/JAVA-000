12988151f659df2c1a80d8337abde14139e97273
jiangchen <cjiang@86links.com>
2020-11-03 20:26:43 +0800

    提交作业

---
+++ Week_03/netty-gateway/src/main/java/io/github/kimmking/gateway/filter/HttpDemoRequestFilter.java
@@ -0,0 +1,18 @@
+package io.github.kimmking.gateway.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+
+/**
+ * @author jiangchen
+ * @date 2020/11/03
+ */
+public class HttpDemoRequestFilter implements HttpRequestFilter {
+
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        HttpHeaders headers = fullRequest.headers();
+        headers.set("nio", "jlif");
+    }
+}
