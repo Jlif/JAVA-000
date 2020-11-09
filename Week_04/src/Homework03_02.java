eccfc9e14d3f6f61c9d44d45ecb3540a99aa0c5f
jlif <jiangchen23@qq.com>
2020-11-10 00:10:34 +0800

    提交作业

--- Week_04/src/Homework03_02.java
+++ Week_04/src/Homework03_02.java
@@ -8,26 +8,34 @@ import java.util.concurrent.*;
  * @author jiangchen
  * @date 2020/11/09
  */
-public class Homework03_01 {
+public class Homework03_02 {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         ExecutorService executorService = new ThreadPoolExecutor(16, 32, 10, TimeUnit.MINUTES, new ArrayBlockingQueue<>(1000));
-        Future<Integer> future = executorService.submit(new Sum());
+        CountDownLatch latch = new CountDownLatch(1);
+        Future<Integer> future = executorService.submit(new Sum(latch));
         executorService.shutdown();
-        System.out.println("拿到结果前，主线程阻塞");
         try {
-            System.out.println("异步计算结果为：" + future.get(5, TimeUnit.SECONDS));
+            latch.await(5, TimeUnit.SECONDS);
+            System.out.println("异步计算结果为：" + future.get());
             System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     static class Sum implements Callable<Integer> {
 
+        private final CountDownLatch latch;
+
+        public Sum(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
         @Override
         public Integer call() {
+            latch.countDown();
             return fibo(36);
         }
 
---
+++ Week_04/src/Homework03_04.java
@@ -0,0 +1,43 @@
+import java.util.concurrent.*;
+
+/**
+ * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
+ * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
+ * 写出你的方法，越多越好，提交到github。
+ *
+ * @author jiangchen
+ * @date 2020/11/09
+ */
+public class Homework03_04 {
+
+    private static volatile int result;
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Thread sub = new Thread(new Sum());
+        sub.start();
+        try {
+            sub.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("异步计算结果为：" + result);
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+
+    }
+
+    static class Sum implements Runnable {
+
+        @Override
+        public void run() {
+            result = fibo(36);
+        }
+
+        private static int fibo(int a) {
+            if (a < 2)
+                return 1;
+            return fibo(a - 1) + fibo(a - 2);
+        }
+
+    }
+}
