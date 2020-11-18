189a3cde3bc0176cfc21022b3fe4a45223f2490a
jlif <jiangchen23@qq.com>
2020-11-18 22:49:34 +0800

    完善作业

--- Week_05/Saturday/spring-boot-starter-demo/src/main/java/tech/jiangchen/starter/DemoAutoConfigure.java
+++ Week_05/Saturday/spring-boot-starter-demo/src/main/java/tech/jiangchen/starter/DemoAutoConfigure.java
@@ -2,7 +2,6 @@ package tech.jiangchen.starter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +20,7 @@ public class DemoAutoConfigure {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(prefix = "demo.starter", value = "enabled", havingValue = "true")
+//    @ConditionalOnProperty(prefix = "demo.starter", value = "enabled", havingValue = "true")
     Student student() {
         return new Student(properties.getId(), properties.getName());
     }
