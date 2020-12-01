a173cff9ad8856f1b1f07faa221fb61856e24430
jlif <jiangchen23@qq.com>
2020-12-01 23:32:50 +0800

    暂时提交

--- Week_07/dynamic-datasource-01/src/main/java/tech/jiangchen/dynamicdatasource01/config/DynamicDataSourceAspect.java
+++ Week_07/dynamic-datasource-01/src/main/java/tech/jiangchen/dynamicdatasource01/config/DynamicDataSourceAspect.java
@@ -3,16 +3,13 @@ package tech.jiangchen.dynamicdatasource01.config;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+@Component
 @Aspect
 public class DynamicDataSourceAspect {
 
-    @Pointcut("@annotation(tech.jiangchen.dynamicdatasource01.config.DS)")
-    public void ds() {
-    }
-
-    @Around("ds() && @annotation(DS)")
+    @Around(value = "@annotation(tech.jiangchen.dynamicdatasource01.config.DS) && @annotation(ds)")
     public void initDB(ProceedingJoinPoint point, DS ds) throws Throwable {
         DataSourceContextHolder.setDB(ds.value());
         point.proceed();
